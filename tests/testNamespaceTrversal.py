4f4f6a411d16b7aa8b7ce7fd452e181e5740085a
Barry Warsaw <barry@python.org>
2002-12-20 19:34:46 +0000

    test module cleanups:
    - no docstrings in test methods (convert to comments)
    - whitespace normalization
    - other minor cleanups

--- tests/testNamespaceTrversal.py
+++ tests/testNamespaceTrversal.py
@@ -2,19 +2,19 @@
 #
 # Copyright (c) 2001, 2002 Zope Corporation and Contributors.
 # All Rights Reserved.
-# 
+#
 # This software is subject to the provisions of the Zope Public License,
 # Version 2.0 (ZPL).  A copy of the ZPL should accompany this distribution.
 # THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
 # WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
 # FOR A PARTICULAR PURPOSE
-# 
+#
 ##############################################################################
 """
 
 Revision information:
-$Id: testNamespaceTrversal.py,v 1.5 2002/07/17 16:54:20 jeremy Exp $
+$Id: testNamespaceTrversal.py,v 1.6 2002/12/20 19:34:44 bwarsaw Exp $
 """
 
 from unittest import TestCase, TestSuite, main, makeSuite
@@ -23,7 +23,7 @@ from Zope.Testing.CleanUp import CleanUp # Base class w registry cleanup
 class C:
     a = 1
     def __getitem__(self, key): return key+'value'
-    
+
 c=C()
 
 
@@ -36,7 +36,6 @@ class Test(CleanUp, TestCase):
         provideNamespaceHandler('attribute', attr)
         provideNamespaceHandler('item', item)
         provideNamespaceHandler('skin', skin)
-
 
     def testAttr(self):
         from Zope.App.Traversing.Traverser import Traverser
@@ -51,27 +50,26 @@ class Test(CleanUp, TestCase):
         self.assertEqual(v, 'avalue')
 
     def testSideEffectsContextDetail(self):
-        """Check to make sure that when we traverse something in context,
-        that we get the right context for the result."""
+        # Check to make sure that when we traverse something in context,
+        # that we get the right context for the result.
         from Zope.Proxy.ContextWrapper \
              import ContextWrapper, getWrapperContainer
         from Zope.App.Traversing.Traverser import Traverser
         from Zope.Publisher.Browser.BrowserRequest import TestRequest
-        
+
         c1 = C()
         c2 = C()
         c2c1 = ContextWrapper(c2, c1)
-        
+
         traverser = Traverser(c2c1)
         v = traverser.traverse('++skin++ZopeTop', request=TestRequest())
         self.assertEqual(v, c2)
         self.failUnless(getWrapperContainer(v) is c2c1)
-        
-        
-        
+
 
 def test_suite():
     return makeSuite(Test)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main(defaultTest='test_suite')

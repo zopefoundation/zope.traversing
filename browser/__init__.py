b811affc0a6b1303b358c37016dbf0aaadcd9a62
Jim Fulton <jim@zope.com>
2006-04-27 00:54:03 +0000

    Merge of jim-adapter branch:
    This branch has three major refactorings on it:
    
    - A redesign of the adapter registration machinery
    
    - A major simplification of local component management
    
      See zope.component.interfaces.IComponentRegistry.
    
    - A flexible system for combining component registries.
    
    - A beginning of the migration of packages out of zope.app.
    
    - A new package for doing deferred imports.  This allows you
      to make names available at the package level without creating
      circular imports.  See zope.deferredimport and see
      zope.component.__init__ for examples of it's use.  This package
      also provides a way to deprecate names in a module.
    
    - Deprecation of zope. i18nmessageid.MessageID
    
    I should have created checkin messages for individual sets of related
    changes, but there were just too many for the time allowed.

--- browser/__init__.py
+++ browser/__init__.py
@@ -1,20 +1,7 @@
-##############################################################################
-#
-# Copyright (c) 2001, 2002 Zope Corporation and Contributors.
-# All Rights Reserved.
-#
-# This software is subject to the provisions of the Zope Public License,
-# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
-# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
-# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
-# FOR A PARTICULAR PURPOSE
-#
-##############################################################################
-"""Absolute URL View components
-
-$Id$
-"""
-
-from zope.app.traversing.browser.absoluteurl import AbsoluteURL
-from zope.app.traversing.browser.absoluteurl import SiteAbsoluteURL
+# This module has moved to zope.traversing.browser
+# and will go away in Zope 3.5
+import zope.deprecation
+zope.deprecation.moved(
+    'zope.traversing.browser',
+    "Zope 3.5",
+    )

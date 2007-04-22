ed4955e003691e829e5a139e01e401545652195a
Christian Theune <ct@gocept.com>
2007-04-22 11:12:42 +0000

    Updating version.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='zope.traversing',
-      version='3.4dev',
+      version = '3.4.0a1',
       url='http://svn.zope.org/zope.traversing',
       license='ZPL 2.1',
       description='Zope traversing',

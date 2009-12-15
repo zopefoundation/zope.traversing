78819f3a09e83901d07b5b38f76a6a22df3e7cd0
Hanno Schlichting <hanno@hannosch.eu>
2009-12-15 19:40:32 +0000

    Update version after release

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ long_description = (open('README.txt').read() +
                     open('CHANGES.txt').read())
 
 setup(name='zope.traversing',
-      version = '3.9.0',
+      version = '3.9.1dev',
       url='http://pypi.python.org/pypi/zope.traversing',
       license='ZPL 2.1',
       author='Zope Corporation and Contributors',

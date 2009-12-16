37e9f8b473a57c26d9148170400ade72c7154c9f
Hanno Schlichting <hanno@hannosch.eu>
2009-12-16 00:48:29 +0000

    Simplified test_dependencies module by using zope.configuration instead of zope.app.testing.functional.

--- src/zope/traversing/tests/test_dependencies.py
+++ src/zope/traversing/tests/test_dependencies.py
@@ -1,34 +1,30 @@
-import os
 import unittest
 
 import zope.component
-from zope.app.testing import functional
+from zope.configuration.xmlconfig import XMLConfig
 from zope.publisher.browser import TestRequest
 
 from zope.traversing.interfaces import ITraversable
-import zope.traversing.namespace
 
 
-TraversingLayer = functional.ZCMLLayer(
-    os.path.join(os.path.dirname(__file__), 'ftest_zcml_dependencies.zcml'),
-    __name__, 'TraversingLayer', allow_teardown=True)
-
-
-class ZCMLDependencies(functional.BrowserTestCase):
+class ZCMLDependencies(unittest.TestCase):
 
     def test_zcml_can_load_with_only_zope_component_meta(self):
-        # this is just an example.  It is supposed to show that the
-        # configure.zcml file has loaded successfully, with only loading the
-        # meta.zcml from zope.component.
+        import zope.component
+        XMLConfig('meta.zcml', zope.component)()
+
+        import zope.traversing
+        XMLConfig('configure.zcml', zope.traversing)()
+
         request = TestRequest()
         res = zope.component.getMultiAdapter(
             (self, request), ITraversable, 'lang')
+        import zope.traversing.namespace
         self.failUnless(isinstance(res, zope.traversing.namespace.lang))
         self.failUnless(res.context is self)
 
 def test_suite():
     suite = unittest.TestSuite()
-    ZCMLDependencies.layer = TraversingLayer
     suite.addTest(unittest.makeSuite(ZCMLDependencies))
     return suite
 

36649cd399efbae8e4cde9ca22326c3ced687dc8
Hanno Schlichting <hanno@hannosch.eu>
2011-03-02 21:41:31 +0000

    Prepare zope.traversing 3.14.0.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ long_description = (open('README.txt').read() +
                     open('CHANGES.txt').read())
 
 setup(name='zope.traversing',
-      version='3.13.2dev',
+      version='3.14.0',
       url='http://pypi.python.org/pypi/zope.traversing',
       license='ZPL 2.1',
       author='Zope Foundation and Contributors',
